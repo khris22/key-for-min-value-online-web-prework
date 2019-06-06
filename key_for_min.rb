da44abccab0ef0c886dbd8ebcf8255e835d9c42e
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-06 12:47:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   key_for_min_value = nil
   lowest_value = nil 
   
-  name.hash each do |key, value|
+  name_hash.each do |key, value|
    if lowest_value == nil || value < lowest_value
      lowest_value = value 
      key_for_min = key
